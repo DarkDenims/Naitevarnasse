38bcc4aaee45a2e1537f0a04008925041d44f378
Xavier Romano <xavieromano1027@gmail.com>
2024-12-29 14:57:36 +0800

    create user login via cli will replace soon

--- userHandling.py
+++ userHandling.py
@@ -31,3 +31,92 @@ def createUser():
     userEmail = input('Enter your email: ')
     # get the user name
     userName = input('Enter your name: ')
+
+
+# Constructing a simple Login
+import json
+from tabulate import tabulate
+
+
+def mainMenu():
+    print("\n\t\tMain Menu\n")
+    choice = input("[S]ignup \n[L]ogin \n[E]xit\nInput: ")
+    if (choice == "L" or choice == "l"):
+        login()
+    elif (choice == "S" or choice == "s"):
+        signup()
+    elif (choice == "E" or choice == "e"):
+        print("...Exiting")
+        exit()
+    else:
+        print("\n\t# Error. Invalid Choice")
+        mainMenu()
+
+
+def login():
+    print("\n\t\tLogin Form\n")
+    counter = 3
+    loggedIn = False
+    while (counter > 0) and (loggedIn == False):
+        credentialUsr = input("Username: ")
+        credentialPsw = input("Password: ")        
+        username = "admin"
+        password = "admin"
+
+        if (credentialUsr == username) and (credentialPsw == password):
+            print("# Login Succesful.")
+            loggedIn = True
+            menu()
+        else:
+            counter -= 1
+            if (counter > 0):
+                print("\n\t# Incorrect Username or Password. Try Again.")
+            else:
+                print("\n\tYou've exceeded the number of tries. Try again later.")
+
+def signup():
+    print("Signup Template")
+
+
+def menu():
+
+    print("\n\t\tUser Dashboard\n")
+    options = input("\n[D]isplay Vault \n[N]ew Credentials \n[A]rchive Credentials\n[L]ogout\n[E]xit\nEnter Option: ")
+    if (options == "D") or (options == "d"):
+        displayVault()
+    elif (options == "N") or (options == "n"):
+        newCreds()
+    elif (options == "A") or (options == "a"):
+        arcCreds()
+    elif (options == "L") or (options == "l"):
+        loggedIn = False
+        mainMenu()
+    elif (options == "E") or (options == "e"):
+        print("...Exiting Program")
+        exit()
+    else:
+        print("Incorrect Option")
+        menu()
+
+
+def displayVault():
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    
+    headers = ["Username", "Password", "Email", "Website"]
+    rows = [[d["username"], d["password"], d["email"], d["website"]] for d in data]
+    
+    # Print data as a table
+    print(tabulate(rows, headers=headers, tablefmt="grid"))
+    menu()
+
+def newCreds():
+    print("New Creds template")
+    menu()
+
+def arcCreds():
+    print("Archive Credentials template")
+    menu()
+
+
+mainMenu()
