9bcd18a5ba4523e23d6a9e429b6c5c57c6b354f8
Xavier Romano <xavieromano1027@gmail.com>
2024-12-31 02:28:50 +0800

    fixed Display Vault, will make separate files soon for each user

--- vault/vaultManager.py
+++ vault/vaultManager.py
@@ -10,20 +10,25 @@ class VaultManager:
         with open(self.data_file, 'r') as file:
             data = json.load(file)
         
-        headers = ["Username", "Password", "Email", "Website"]
-        rows = [[d["username"], d["password"], d["email"], d["website"]] 
-               for d in data]
+        users = data.get("users", [])
+        
+        headers = ["Username", "Password", "Email"]
+        rows = [[
+                d.get("username", ""),
+                d.get("password", ""),
+                d.get("email", "")
+            ] for d in users]
+
         
         print(tabulate(rows, headers=headers, tablefmt="grid"))
 
-    def add_credentials(self, username, password, email, website):
+    def add_credentials(self, username, password, email):
         with open(self.data_file, 'r+') as file:
             data = json.load(file)
             data.append({
                 "username": username,
                 "password": password,
                 "email": email,
-                "website": website
             })
             file.seek(0)
             json.dump(data, file, indent=4)
